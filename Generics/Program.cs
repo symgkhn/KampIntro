0b0b472026c2989839c2ee8d3aed273f2e1e0343
symgkhn <seymagokhan0@gmail.com>
2021-03-27 12:23:02 +0300

    KampIntro

---
+++ Generics/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+
+            Console.WriteLine(sehirler.Count);
+            
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            Console.WriteLine(sehirler2.Count);
+            
+        }
+    }
+    class MyList<T> //Generic class
+    {
+        T[] array;
+        T[] tempArray;
+        public MyList()
+        {
+            array = new T[0];
+        }
+        public void Add(T item)
+        {
+            tempArray = array;
+            array = new T[array.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                array[i] = tempArray[i];
+
+            }
+            array[array.Length - 1] = item;
+        }
+        
+
+        public int Count
+        {
+            get { return array.Length; }
+            
+        }
+
+    }
+}
