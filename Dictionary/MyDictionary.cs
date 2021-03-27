11a22ee84c5ead6c391a53ca682571a458130661
symgkhn <seymagokhan0@gmail.com>
2021-03-27 13:15:52 +0300

    DictionaryTask

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    public class MyDictionary<TKey , SValue>
+    {
+        TKey[] key;
+        SValue[] value;
+
+        public MyDictionary()
+        {
+            key = new TKey[0];
+            value = new SValue[0];
+
+        }
+        public void Add(TKey Key,SValue Value)
+        {
+            TKey[] tempkey = key;
+            SValue[] tempvalue = value;
+            key = new TKey[key.Length + 1];
+            value = new SValue[value.Length + 1];
+
+            for (int i = 0; i < tempkey.Length; i++)
+            {
+                key[i] = tempkey[i];
+            }
+            for (int i = 0; i < tempvalue.Length; i++)
+            {
+                value[i] = tempvalue[i];
+            }
+
+            key[key.Length - 1] = Key;
+            value[value.Length - 1] = Value;
+
+        }
+        public int Length
+        {
+            get { return key.Length; }
+        }
+        public TKey[] _Key
+        {
+            get { return key; }
+        }
+     
+
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, string> agesOfPeople = new Dictionary<int, string>();
+            agesOfPeople.Add( 16, "Ahmet");
+            agesOfPeople.Add(34, "Mehmet");
+            agesOfPeople.Add( 24, "Ali");
+            agesOfPeople.Add( 37, "Ayşe");
+            agesOfPeople.Add(21, "Fatma");
+
+            foreach (var age in agesOfPeople)
+            {
+                Console.WriteLine(age);
+            }
+
+            
+        }
+    }
+}
